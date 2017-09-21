05ae9d6046ed5f3209f600f04d5c1da30f66cb7d
Atzkdeff <a.shyliahin@gmail.com>
2017-09-22 00:36:35 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+
+    this.checkThere = function (str) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str == bracketsConfig[i][0] || str == bracketsConfig[i][1]) return true
+        }
+        return false
+    };
+
+    this.checkEqual = function (str) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str == bracketsConfig[i][0] && bracketsConfig[i][0] == bracketsConfig[i][1]) {
+                return true
+            }
+        }
+        return false
+    };
+
+    this.equalDelAdd = function (str){
+        if (stack[stack.length - 1] == str) {
+            stack.pop();
+        }else {
+            stack.push(str);
+        }
+    };
+
+    this.checkLeft = function (str) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str == bracketsConfig[i][0]) return true
+        }
+        return false
+    };
+
+    this.checkRight = function (str) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str == bracketsConfig[i][1] && stack[stack.length-1] == bracketsConfig[i][0]) {
+
+                return true
+            }
+        }
+        return false
+    };
+
+    for (let i = 0; i < str.length; i++) {
+        let k = str.charAt(i);
+        if (!this.checkThere(k)) {
+            return false
+        } else if (this.checkEqual(k)) {
+            this.equalDelAdd(k);
+            continue
+        } else if (this.checkLeft(k)) {
+            stack.push(k);
+            continue
+        } else if (this.checkRight(k)) {
+            stack.pop();
+            continue
+        };
+        return false
+    }
+    return stack.length == 0
+};
+
+
+
+
